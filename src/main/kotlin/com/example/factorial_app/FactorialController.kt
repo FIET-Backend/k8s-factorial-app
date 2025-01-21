27b077886213fa1f697c97024cfe3b9c1a0c1923
youngwoo-song <yw.song@fiet.net>
2025-01-21 11:45:01 +0900

    k8s deploy

--- src/main/kotlin/com/example/factorial_app/FactorialController.kt
+++ src/main/kotlin/com/example/factorial_app/FactorialController.kt
@@ -1,5 +1,7 @@
 package com.example.factorial_app
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -7,8 +9,9 @@ import java.math.BigDecimal
 
 @RestController
 class FactorialController(
-    private val factorialService: FactorialService
+    private val factorialService: FactorialService,
 ) {
+    private val logger: Logger = LoggerFactory.getLogger(FactorialController::class.java)
 
     @GetMapping("/factorial")
     fun factorial(
@@ -17,6 +20,9 @@ class FactorialController(
         if(n < 0) {
             throw IllegalArgumentException("n must be non-negative")
         }
+
+        logger.info("Calculating factorial of $n")
+
         return factorialService.calculate(n)
     }
 }
